44966ba119e4ada7b8f7e9553e6aff44aa65079d
Matthew Davey <reapazor@gmail.com>
2017-05-14 00:49:12 -0400

    WIP Scrape

--- src/handlers/meetup.go
+++ src/handlers/meetup.go
@@ -1,8 +1,13 @@
 package handlers
 
 import (
+	"strings"
+
+	"fmt"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/mmcdole/gofeed"
+	"golang.org/x/net/html"
 )
 
 func HandleMeetup(session *discordgo.Session, message *discordgo.MessageCreate) {
@@ -10,30 +15,30 @@ func HandleMeetup(session *discordgo.Session, message *discordgo.MessageCreate) 
 	fp := gofeed.NewParser()
 	feed, _ := fp.ParseURL("https://www.meetup.com/PTBOGameDev/events/rss/")
 
-	//parsedHTML := html.NewTokenizer(strings.NewReader(feed.Items[0].Description))
+	parsedHTML := html.NewTokenizer(strings.NewReader(feed.Items[0].Description))
 
-	imageURL := "https://secure.meetupstatic.com/photos/event/1/1/2/9/event_457804393.jpeg"
+	imageURL := ""
 
-	// for {
-	// 	currentToken := parsedHTML.Next()
-
-	// 	switch {
-	// 	case currentToken == html.ErrorToken:
-	// 		// End of the document, we're done
-	// 		break
-	// 	case currentToken == html.StartTagToken:
-	// 		tag := parsedHTML.Token()
-	// 		isImage := tag.Data == "img"
-	// 		if isImage {
-	// 			for _, a := range tag.Attr {
-	// 				if a.Key == "src" {
-	// 					imageURL = a.Val
-	// 					continue
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// }
+	currentToken := parsedHTML.Next()
+	for currentToken != html.ErrorToken {
+		switch {
+		case currentToken == html.StartTagToken:
+			tag := parsedHTML.Token()
+			fmt.Printf(tag.Data)
+			isImage := tag.Data == "img"
+			if isImage {
+				fmt.Printf("FOUND IMAGE")
+				for _, a := range tag.Attr {
+					if a.Key == "src" {
+						fmt.Printf("FOUND SRC")
+						imageURL = a.Val
+						continue
+					}
+				}
+			}
+		}
+		currentToken = parsedHTML.Next()
+	}
 
 	_, _ = session.ChannelMessageSendEmbed(message.ChannelID, &discordgo.MessageEmbed{
 		Type:        "rich",
@@ -57,11 +62,11 @@ func HandleMeetup(session *discordgo.Session, message *discordgo.MessageCreate) 
 	/*<description>
 	<![CDATA[
 		<p>
-		<img style="float:left; margin-right:4px" src="https://secure.meetupstatic.com/photos/event/1/1/2/9/event_457804393.jpeg" alt="photo" class="photo" />
-		PTBO Game Development</p>
+			<img style="float:left; margin-right:4px" src="https://secure.meetupstatic.com/photos/event/1/1/2/9/event_457804393.jpeg" alt="photo" class="photo" />
+			PTBO Game Development
+		</p>
 		<p><p>
 		<span>You can show up anytime after 6:30PM, we're just calling 7PM the official start time.</span>
-
 		</p> <br></p>
 		<p>K9J 7B1, ON   - Canada</p>
 		<p>Wednesday, May 17 at 7:00 PM</p> <p>14</p> <p>https://www.meetup.com/PTBOGameDev/events/237855732/</p> ]]></description>*/
