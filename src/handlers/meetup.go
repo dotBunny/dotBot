c099ea20d2920ca1a2eb102b28f5b16c54d39c86
Matthew Davey <reapazor@gmail.com>
2017-05-14 08:05:55 -0400

    Meetup code working

--- src/handlers/meetup.go
+++ src/handlers/meetup.go
@@ -1,13 +1,9 @@
 package handlers
 
 import (
-	"strings"
-
-	"fmt"
-
+	Helpers "../helpers"
 	"github.com/bwmarrin/discordgo"
 	"github.com/mmcdole/gofeed"
-	"golang.org/x/net/html"
 )
 
 func HandleMeetup(session *discordgo.Session, message *discordgo.MessageCreate) {
@@ -15,29 +11,18 @@ func HandleMeetup(session *discordgo.Session, message *discordgo.MessageCreate) 
 	fp := gofeed.NewParser()
 	feed, _ := fp.ParseURL("https://www.meetup.com/PTBOGameDev/events/rss/")
 
-	parsedHTML := html.NewTokenizer(strings.NewReader(feed.Items[0].Description))
+	foundImage, imageElement := Helpers.GetElement("img", feed.Items[0].Description, 0)
+	foundTimeBlock, timeElement := Helpers.GetElementContent("p", feed.Items[0].Description, 4)
 
-	imageURL := ""
+	imageSource := "https://secure.meetupstatic.com/photos/event/1/1/2/9/event_457804393.jpeg"
+	nextEvent := "Unknown"
 
-	currentToken := parsedHTML.Next()
-	for currentToken != html.ErrorToken {
-		switch {
-		case currentToken == html.StartTagToken:
-			tag := parsedHTML.Token()
-			fmt.Printf(tag.Data)
-			isImage := tag.Data == "img"
-			if isImage {
-				fmt.Printf("FOUND IMAGE")
-				for _, a := range tag.Attr {
-					if a.Key == "src" {
-						fmt.Printf("FOUND SRC")
-						imageURL = a.Val
-						continue
-					}
-				}
-			}
-		}
-		currentToken = parsedHTML.Next()
+	if foundTimeBlock {
+		nextEvent = timeElement
+	}
+
+	if foundImage {
+		_, imageSource = Helpers.GetAttribute("src", imageElement)
 	}
 
 	_, _ = session.ChannelMessageSendEmbed(message.ChannelID, &discordgo.MessageEmbed{
@@ -49,25 +34,13 @@ func HandleMeetup(session *discordgo.Session, message *discordgo.MessageCreate) 
 		Fields: []*discordgo.MessageEmbedField{
 			&discordgo.MessageEmbedField{
 				Name:   "Next Event",
-				Value:  "Wednesday, May 17 at 7:00 PM",
+				Value:  nextEvent,
 				Inline: true},
 			&discordgo.MessageEmbedField{
 				Name:   "Location",
 				Value:  "KTTC\nFleming College\nRoom D1111",
 				Inline: true}},
 		Thumbnail: &discordgo.MessageEmbedThumbnail{
-			URL: imageURL},
+			URL: imageSource},
 	})
-
-	/*<description>
-	<![CDATA[
-		<p>
-			<img style="float:left; margin-right:4px" src="https://secure.meetupstatic.com/photos/event/1/1/2/9/event_457804393.jpeg" alt="photo" class="photo" />
-			PTBO Game Development
-		</p>
-		<p><p>
-		<span>You can show up anytime after 6:30PM, we're just calling 7PM the official start time.</span>
-		</p> <br></p>
-		<p>K9J 7B1, ON   - Canada</p>
-		<p>Wednesday, May 17 at 7:00 PM</p> <p>14</p> <p>https://www.meetup.com/PTBOGameDev/events/237855732/</p> ]]></description>*/
 }
---
+++ src/helpers/html.go
@@ -0,0 +1,108 @@
+package handlers
+
+import (
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+// GetAttribute returns the attribute value in a token
+func GetAttribute(attr string, token html.Token) (ok bool, value string) {
+	// Iterate over all of the Token's attributes until we find an "href"
+	for _, a := range token.Attr {
+		if a.Key == attr {
+			return true, a.Val
+		}
+	}
+	return false, ""
+}
+
+// GetElementContent will look for the following text nodes content
+func GetElementContent(tag string, content string, offset int) (found bool, result string) {
+
+	foundTag := false
+	z := html.NewTokenizer(strings.NewReader(content))
+
+	for {
+		tt := z.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			// End of the document, we're done
+			return
+		case tt == html.SelfClosingTagToken:
+			t := z.Token()
+			if t.Data == tag {
+				if offset == 0 {
+					foundTag = true
+				}
+				offset--
+			}
+		case tt == html.TextToken:
+			t := z.Token()
+
+			if t.Data == tag {
+				if offset == 0 {
+					foundTag = true
+				}
+				offset--
+			}
+			if foundTag {
+				return true, t.Data
+			}
+		case tt == html.StartTagToken:
+			t := z.Token()
+			if t.Data == tag {
+				if offset == 0 {
+					foundTag = true
+				}
+				offset--
+			} else {
+				if foundTag {
+					foundTag = false
+				}
+			}
+		}
+	}
+
+}
+
+// GetElement returns the first tag found with the selector
+func GetElement(tag string, content string, offset int) (found bool, element html.Token) {
+
+	z := html.NewTokenizer(strings.NewReader(content))
+
+	for {
+		tt := z.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			// End of the document, we're done
+			return
+		case tt == html.SelfClosingTagToken:
+			t := z.Token()
+			if t.Data == tag {
+				if offset == 0 {
+					return true, t
+				}
+				offset--
+			}
+		case tt == html.TextToken:
+			t := z.Token()
+			if t.Data == tag {
+				if offset == 0 {
+					return true, t
+				}
+				offset--
+			}
+		case tt == html.StartTagToken:
+			t := z.Token()
+			if t.Data == tag {
+				if offset == 0 {
+					return true, t
+				}
+				offset--
+			}
+		}
+	}
+}
